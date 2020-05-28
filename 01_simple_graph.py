d8e35b89539268174812a0d3d57610744717d3f1
John Cabiles <johncabiles@gmail.com>
2020-05-28 00:30:35 -0700

    Import trends data then plot

---
+++ 01_simple_graph.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# row 1 is useless so we can skip it to get the correct headers
+# let's also make sure that 'Week' is used as the index
+diet = pd.read_csv("diet_google_trends.csv", skiprows=1, index_col=0)
+diet.index = pd.to_datetime(diet.index)
+
+# plot
+diet.plot(grid=True)
+plt.show()
